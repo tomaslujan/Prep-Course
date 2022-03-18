0bde0ea902ef45c4eac4b922a8af9661daac6870
tomaslujan <tomylujann13@gmail.com>
2022-03-18 13:40:42 -0300

    tom

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -245,7 +245,11 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra=='a'||letra=='e'||letra=='i'||letra=='o'||letra=='u'){
+    return "es vocal";
+  }else if (letra.length !==1){
+    return "dato incorrecto"
+  }
 }
 
 
